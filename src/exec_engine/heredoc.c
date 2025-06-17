293b16c4caf74d679bc100b0241d13de84bf9d98
elitebook <matus.gavornik@gmail.com>
2025-06-17 09:17:01 +0200

    new update working, what i suposed to make work, probbably bad version of everything else, need to choose what to update

--- src/exec_engine/heredoc.c
+++ src/exec_engine/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: mgavornik <mgavornik@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/23 22:27:53 by vnicoles          #+#    #+#             */
-/*   Updated: 2025/06/13 19:52:36 by mgavornik        ###   ########.fr       */
+/*   Updated: 2025/06/17 04:11:43 by mgavornik        ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -20,96 +20,53 @@ static int	status_check(int *pipe_fds, int status, char *expanded, char *line)
 		free(line);
 		close(pipe_fds[0]);
 		close(pipe_fds[1]);
-
 		return (-1);
 	}
 	return (0);
 }
-static bool herdoc_stop(char *line, char *delimiter)
+static bool	herdoc_stop(char *line, char *delimiter)
 {
-    if (!line)
-        return (true);
-    if (ft_strcmp(line, delimiter) == 0)
-        return (true);   
-    return (false);
+	if (!line)
+		return (true);
+	if (ft_strcmp(line, delimiter) == 0)
+		return (true);
+	return (false);
 }
-int process_heredoc_input(t_env *env, char *delimiter, int write_fd)
+static int expanded_herdoc(char *expanded, ssize_t status, int write_fd, char *line)
 {
-    char    *line;
-    char    *expanded;
-    ssize_t status = 0;
-
-    while (1)
-    {
-        line = readline("heredoc> ");
-        if (herdoc_stop(line, delimiter))
-        {
-            if (!line)
-                fprintf(stderr, "minishell: warning: here-document delimited by end-of-file (wanted `%s')\n", delimiter);
-            free(line);
-            break;
-        }
-        expanded = expand_var(env, line);
-        if (expanded)
-        {
-            status = write(write_fd, expanded, ft_strlen(expanded));
-            if (status == -1 || write(write_fd, "\n", 1) == -1)
-            {
-                perror("write failed");
-				status = status_check(&write_fd, status, expanded, line);
-                return (status);
-            }
-            free(expanded);
-        }
-        free(line);
-    }
-    return (0);
+	status = write(write_fd, expanded, ft_strlen(expanded));
+	if (status == -1 || write(write_fd, "\n", 1) == -1)
+	{
+		perror("write failed");
+		status = status_check(&write_fd, status, expanded, line);
+	}
+	return (status);
 }
+int	process_heredoc_input(t_env *env, char *delimiter, int write_fd)
+{
+	char	*line;
+	char	*expanded;
+	ssize_t	status;
 
-// int process_heredoc_input(t_env *env, char *delimiter, int write_fd)
-// {
-//     char    *line;
-//     char    *expanded;
-//     ssize_t status;
-    
-//     status = 0;
-//     line = NULL;
-//     while (1)
-//     {
-//         // if(sig > 0)
-//         // {
-//         //     mini_sigint_handler(sig);
-//         //     return (-1);
-//         // }
-        
-//        // rl_cleanup_after_signal();
-//         line = readline("heredoc> ");
-        
-//         // Check if we should stop (delimiter found or EOF)
-//         if (herdoc_stop(line, delimiter))
-//         {
-//             if (!line) // This is specifically the EOF case
-//             {
-//                 fprintf(stderr, "minishell: warning: here-document delimited by end-of-file (wanted `%s')\n", delimiter);
-//             }
-            
-//             free(line);
-//             // break; // Actually break out of the loop!
-//         }
-//         expanded = expand_var(env, line);
-//         if (expanded)
-//         {
-//             status = write(write_fd, expanded, ft_strlen(expanded));
-//             if (status == -1 || write(write_fd, "\n", 1) == -1)
-//             {
-//                 status_check(NULL, status, expanded, line);
-//                 return (-1);
-//             }
-//             free(expanded);
-//         }
-//         free(line);
-//     }
-    
-//     return (status);
-// }
-
+	status = 0;
+	while (1)
+	{
+		line = readline("heredoc> ");
+		if (herdoc_stop(line, delimiter))
+		{
+			if (!line)
+				printf("minishell: warning: here-document delimited by end-of-file (wanted `%s')\n",
+					delimiter);
+			free(line);
+			break ;
+		}
+		expanded = expand_var(env, line);
+		if (expanded)
+		{
+			status = expanded_herdoc(expanded, status, write_fd, line);
+			free(expanded);	
+		}
+		free(line);
+	}
+	return (0);
+}
