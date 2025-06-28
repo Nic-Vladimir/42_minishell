9f23b1e78225e1bd0fe6acbb480910a128bc1e5a
elitebook <matus.gavornik@gmail.com>
2025-06-28 16:14:12 +0200

    just env->last_sig is empty so leak or some fuck)

--- src/exec_engine/pedo.c
+++ src/exec_engine/pedo.c
@@ -1,14 +1,14 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   pedo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mgavorni <mgavorni@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mgavornik <mgavornik@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/05 12:39:37 by mgavornik         #+#    #+#             */
-/*   Updated: 2025/06/22 21:40:49 by mgavorni         ###   ########.fr       */
+/*   Updated: 2025/06/28 16:13:03 by mgavornik        ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "../../inc/minishell.h"
 #include "../../inc/pedo.h"
@@ -38,7 +38,7 @@ static void	custom_handler(int sig, siginfo_t *info, void *ucontext)
 	if (sig == SIGINT)
 	{
 		mini_sigint_handler(sig);
-		execute_exit(env, env->last_exit_code);
+		execute_exit(env, 130);
 	}
 }
 
