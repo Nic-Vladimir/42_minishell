5d7178cee64b62f005ef0ba0c3af054a0a0333e6
elitebook <matus.gavornik@gmail.com>
2025-08-14 10:49:43 +0200

    splitted execute_pipeline_cmds() to norm standars...read also prev commits for more info

--- src/exec_engine/exec_engine.c
+++ src/exec_engine/exec_engine.c
@@ -6,7 +6,7 @@
 /*   By: mgavornik <mgavornik@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 08:34:17 by vnicoles          #+#    #+#             */
-/*   Updated: 2025/08/14 10:30:47 by mgavornik        ###   ########.fr       */
+/*   Updated: 2025/08/14 10:44:22 by mgavornik        ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -119,57 +119,117 @@ int	wait_for_children(t_env *env, pid_t last_pid, int pipeline_count)
 	}
 	return (last_cmd_status);
 }
-
-int	execute_pipeline_cmds(t_env *env, t_list *pipeline)
+static int setup_pipe(t_list *pipeline, int *pipe_fd)
 {
-	int		pipe_fd[2];
-	int		prev_read_end;
-	pid_t	last_pid;
-	int		last_cmd_status;
-	int		pipeline_count;
-
-	if (!pipeline)
-		return (1);
-	pipeline_count = ft_lstsize(pipeline);
-	last_pid = 0;
-	prev_read_end = -1;
-	env->pipeline = pipeline;
-	while (pipeline)
-	{
-		if (pipeline->next)
-		{
-			if (pipe(pipe_fd) == -1)
-			{
-				perror("pipe");
-				return (1);
-			}
-		}
-		else
-		{
-			pipe_fd[READ_END] = -1;
-			pipe_fd[WRITE_END] = -1;
-		}
-		last_pid = execute_one_pipeline_cmd(env, pipeline, prev_read_end,
-				pipe_fd);
-		if (last_pid == -1)
-		{
-			if (pipeline->next)
-			{
-				close(pipe_fd[READ_END]);
-				close(pipe_fd[WRITE_END]);
-			}
-			return (1);
-		}
-		if (pipeline->next)
-			prep_for_next_cmd(env, &prev_read_end, pipe_fd);
-		else if (prev_read_end != -1)
-			close(prev_read_end);
-		pipeline = pipeline->next;
-	}
-	last_cmd_status = wait_for_children(env, last_pid, pipeline_count);
-	printf("\n");
-	return (last_cmd_status);
+    if (pipeline->next)
+    {
+        if (pipe(pipe_fd) == -1)
+        {
+            perror("pipe");
+            return 1;
+        }
+    }
+    else
+    {
+        pipe_fd[READ_END] = -1;
+        pipe_fd[WRITE_END] = -1;
+    }
+    return 0;
 }
+static int handle_pipeline_error(t_list *pipeline, int *pipe_fd)
+{
+    if (pipeline->next)
+    {
+        close(pipe_fd[READ_END]);
+        close(pipe_fd[WRITE_END]);
+    }
+    return 1;
+}
+static int process_pipeline_cmd(
+    t_env *env, t_list *pipeline, int *prev_read_end, pid_t *last_pid)
+{
+    int pipe_fd[2];
+    if (setup_pipe(pipeline, pipe_fd))
+        return 1;
+    *last_pid = execute_one_pipeline_cmd(env, pipeline, *prev_read_end, pipe_fd);
+    if (*last_pid == -1)
+        return handle_pipeline_error(pipeline, pipe_fd);
+    if (pipeline->next)
+        prep_for_next_cmd(env, prev_read_end, pipe_fd);
+    else if (*prev_read_end != -1)
+        close(*prev_read_end);
+    return 0;
+}
+int execute_pipeline_cmds(t_env *env, t_list *pipeline)
+{
+    int prev_read_end = -1;
+    pid_t last_pid = 0;
+    int pipeline_count, last_cmd_status;
+    if (!pipeline)
+        return 1;
+    pipeline_count = ft_lstsize(pipeline);
+    env->pipeline = pipeline;
+    t_list *current = pipeline;
+    while (current)
+    {
+        if (process_pipeline_cmd(env, current, &prev_read_end, &last_pid))
+            return 1;
+        current = current->next;
+    }
+    last_cmd_status = wait_for_children(env, last_pid, pipeline_count);
+    printf("\n");
+    return last_cmd_status;
+}
+// int	execute_pipeline_cmds(t_env *env, t_list *pipeline)
+// {
+// 	int		pipe_fd[2];
+// 	int		prev_read_end;
+// 	pid_t	last_pid;
+// 	int		last_cmd_status;
+// 	int		pipeline_count;
+
+// 	if (!pipeline)
+// 		return (1);
+// 	pipeline_count = ft_lstsize(pipeline);
+// 	last_pid = 0;
+// 	prev_read_end = -1;
+// 	env->pipeline = pipeline;
+// 	while (pipeline)
+// 	{
+// 		if (pipeline->next)
+// 		{
+// 			if (pipe(pipe_fd) == -1)
+// 			{
+// 				perror("pipe");
+// 				return (1);
+// 			}
+// 		}
+// 		else
+// 		{
+// 			pipe_fd[READ_END] = -1;
+// 			pipe_fd[WRITE_END] = -1;
+// 		}
+// 		last_pid = execute_one_pipeline_cmd(env, pipeline, prev_read_end,
+// 				pipe_fd);
+// 		if (last_pid == -1)
+// 		{
+// 			if (pipeline->next)
+// 			{
+// 				close(pipe_fd[READ_END]);
+// 				close(pipe_fd[WRITE_END]);
+// 			}
+// 			return (1);
+// 		}
+// 		if (pipeline->next)
+// 			prep_for_next_cmd(env, &prev_read_end, pipe_fd);
+// 		else if (prev_read_end != -1)
+// 			close(prev_read_end);
+// 		pipeline = pipeline->next;
+// 	}
+// 	last_cmd_status = wait_for_children(env, last_pid, pipeline_count);
+// 	printf("\n");
+// 	return (last_cmd_status);
+// }
 
 void	free_pipeline_list(t_list **pipeline)
 {
